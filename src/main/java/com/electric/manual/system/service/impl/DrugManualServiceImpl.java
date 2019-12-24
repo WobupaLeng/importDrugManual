ffed40f7314d13881a0930471de6dd29f95613da
edgar <electric.qiu@hotmail.com>
2019-12-24 21:51:15 +0800

    add sql and pojo

---
+++ src/main/java/com/electric/manual/system/service/impl/DrugManualServiceImpl.java
@@ -0,0 +1,12 @@
+package com.electric.manual.system.service.impl;
+
+import com.electric.manual.system.entity.DrugManual;
+import com.electric.manual.system.dao.DrugManualMapper;
+import com.electric.manual.system.service.IDrugManualService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DrugManualServiceImpl extends ServiceImpl<DrugManualMapper, DrugManual> implements IDrugManualService {
+
+}
